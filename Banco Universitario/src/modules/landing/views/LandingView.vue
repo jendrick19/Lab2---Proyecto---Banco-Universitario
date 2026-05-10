ef43e6e85198f2ab745933ef8b2dcc27484eb0cc
Claraaap <1001.30405569.ucla@gmail.com>
2026-05-10 12:26:44 -0600

    sección beneficios agregada

--- Banco Universitario/src/modules/landing/views/LandingView.vue
+++ Banco Universitario/src/modules/landing/views/LandingView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeroSection from '@/modules/landing/components/HeroSection.vue'
 import NosotrosSection from '@/modules/landing/components/NosotrosSection.vue'
+import BenefitsSection from '@/modules/landing/components/BenefitsSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <NosotrosSection />
+  <BenefitsSection />
 </template>
