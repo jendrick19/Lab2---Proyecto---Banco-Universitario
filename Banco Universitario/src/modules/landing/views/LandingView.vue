227f6bf293bea695fc451729d03a804e6b6153c1
Carani Rodriguez <caranirodriguezlameda@gmail.com>
2026-05-10 20:34:06 -0400

    feat: implementar estructura de tarjetas para la sección de servicios

--- Banco Universitario/src/modules/landing/views/LandingView.vue
+++ Banco Universitario/src/modules/landing/views/LandingView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeroSection from '@/modules/landing/components/HeroSection.vue'
 import NosotrosSection from '@/modules/landing/components/NosotrosSection.vue'
+import ServiciosSection from '@/modules/landing/components/ServiciosSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <NosotrosSection />
+  <ServiciosSection />
 </template>
