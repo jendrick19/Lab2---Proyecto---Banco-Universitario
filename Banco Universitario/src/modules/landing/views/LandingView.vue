f1e163fe88f962b3a3e7271f32ebc75c8cde3621
Sebastian-CR24 <sebastian.castillo@cidesagroup.com>
2026-05-14 18:58:45 -0400

    Nueva seccion de testimonios

--- Banco Universitario/src/modules/landing/views/LandingView.vue
+++ Banco Universitario/src/modules/landing/views/LandingView.vue
@@ -3,6 +3,7 @@ import HeroSection from '@/modules/landing/components/HeroSection.vue'
 import NosotrosSection from '@/modules/landing/components/NosotrosSection.vue'
 import BenefitsSection from '@/modules/landing/components/BenefitsSection.vue'
 import StatsSection from '@/modules/landing/components/Stats.vue'
+import Testimonials from '@/modules/landing/components/Testimonials.vue'
 import FAQ from '@/modules/landing/components/FAQ.vue'
 </script>
 
@@ -11,5 +12,6 @@ import FAQ from '@/modules/landing/components/FAQ.vue'
   <NosotrosSection />
   <BenefitsSection />
   <StatsSection />
+  <Testimonials />
   <FAQ />
 </template>
